d3f76fc08ea1233dfb837789f7e330277f876042
Adii Code <adityaproworks@gmail.com>
2024-07-22 05:52:59 +0530

    Make Some Changes

--- src/component/ChallengeDetails.jsx
+++ src/component/ChallengeDetails.jsx
@@ -10,10 +10,10 @@ const ChallengeDetails = () => {
     if (!challenge) return <p>Challenge not found</p>;
 
     return (
-        <div className="container mx-auto px-8 flex flex-col sm:w-[100%] items-center py-8">
+        <div className="container mx-auto px-8 flex flex-col w-[100%] items-center py-8">
             <p className="text-xl font-bold">Day - {challenge.day}</p>
             <h1 className="text-3xl font-bold mb-8">Topic - {challenge.topic}</h1>
-            <img src={challenge.image} alt={challenge.topic} className="w-full h-screen object-cover mb-4 mt-3" />
+            <img src={challenge.image} alt={challenge.topic} className="w-full object-cover mb-4 mt-3" />
             <Link to={challenge.gitrepo}>
                 <Button>Check Solution</Button>
             </Link>
