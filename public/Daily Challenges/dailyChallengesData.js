26c5407c138c040720eed9acfc0742ff420f29f2
Adii Code <adityaproworks@gmail.com>
2024-07-23 22:24:30 +0530

    Add Day 11 Challenges

--- public/Daily Challenges/dailyChallengesData.js
+++ public/Daily Challenges/dailyChallengesData.js
@@ -69,12 +69,13 @@ export const challengeData = [
         image: "/Images/Day10.png",
         gitrepo: "https://github.com/ad1tyayadav/30-days-javascript-challenge/tree/main/10%20-%20Event%20Handling"
     },
-    // {
-    //     id: 11,
-    //     day: 11,
-    //     topic: "Fetch API",
-    //     image: "https://images.unsplash.com/photo-1593706629823-759d23b35c72?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    // },
+    {
+        id: 11,
+        day: 11,
+        topic: "Promises And Async:Await",
+        image: "/Images/Day11.png",
+        gitrepo: "https://github.com/ad1tyayadav/30-days-javascript-challenge/tree/main"
+    },
     // {
     //     id: 12,
     //     day: 12,
