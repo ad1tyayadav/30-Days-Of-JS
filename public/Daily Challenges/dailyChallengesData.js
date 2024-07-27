d30532059c6daba58e4342a6ae0c1e34efb8626e
Adii Code <adityaproworks@gmail.com>
2024-07-27 15:43:53 +0530

    Add Day 15 Challenges

--- public/Daily Challenges/dailyChallengesData.js
+++ public/Daily Challenges/dailyChallengesData.js
@@ -97,12 +97,13 @@ export const challengeData = [
         image: "/Images/Day14.png",
         gitrepo: "https://github.com/ad1tyayadav/30-days-javascript-challenge/tree/main"
     },
-    // {
-    //     id: 15,
-    //     day: 15,
-    //     topic: "Regular Expressions",
-    //     image: "https://images.unsplash.com/photo-1586860668763-f209a165bc44?q=80&w=1917&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8fHx8fGVufDB8fHx8fA%3D%3D"
-    // },
+    {
+        id: 15,
+        day: 15,
+        topic: "Closures",
+        image: "/Images/Day15.png",
+        gitrepo: "https://github.com/ad1tyayadav/30-days-javascript-challenge/tree/main"
+    },
     // {
     //     id: 16,
     //     day: 16,
