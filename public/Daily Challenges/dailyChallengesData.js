ea61fbfcdb37f74afe5ad7e5d540015ae3283db4
Adii Code <adityaproworks@gmail.com>
2024-07-30 10:46:14 +0530

    Add Day 18 Challenges

--- public/Daily Challenges/dailyChallengesData.js
+++ public/Daily Challenges/dailyChallengesData.js
@@ -117,13 +117,13 @@ export const challengeData = [
         topic: "Data Structures",
         image: "/Images/Day17.png",
         gitrepo: "https://github.com/ad1tyayadav/30-days-javascript-challenge/tree/main"
-    }
-    // {
-    //     id: 18,
-    //     day: 18,
-    //     topic: "Prototypes and Inheritance",
-    //     image: "https://images.unsplash.com/photo-1582591649302-6bb0ae76a558?q=80&w=1917&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    // },
+    },
+    {
+        id: 18,
+        day: 18,
+        topic: "Algorithms",
+        image: "/Images/Day18.png"
+    },
     // {
     //     id: 19,
     //     day: 19,
