4845bdd82d6f6206f28acb3bc5b07dd9ece73ffc
Adii Code <adityaproworks@gmail.com>
2024-07-31 19:54:21 +0530

    Add Day 19 Challenges

--- public/Daily Challenges/dailyChallengesData.js
+++ public/Daily Challenges/dailyChallengesData.js
@@ -124,12 +124,12 @@ export const challengeData = [
         topic: "Algorithms",
         image: "/Images/Day18.png"
     },
-    // {
-    //     id: 19,
-    //     day: 19,
-    //     topic: "Classes and Objects",
-    //     image: "https://images.unsplash.com/photo-1617860124576-34b17b2b7c46?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    // },
+    {
+        id: 19,
+        day: 19,
+        topic: "Regular Expressions",
+        image: "/Images/Day19.png"
+    },
     // {
     //     id: 20,
     //     day: 20,
